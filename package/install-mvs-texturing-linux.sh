9ac66ac5e9528e14ccf2fc65d031f953144d4c3a
Jiahao620 <1023499614@qq.com>
2025-09-24 11:59:39 +0800

    fix: add missing external/mvs-texturing source

--- package/install-mvs-texturing-linux.sh
+++ package/install-mvs-texturing-linux.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-set -e
-mkdir -p "$(dirname "$0")/../external"
-cd "$(dirname "$0")/../external/mvs-texturing"
-mkdir -p build
-cd build
+set -ex      
+ROOT="$(dirname "$0")/.."
+EXT="$ROOT/external"
+mkdir -p "$EXT"
+cd "$EXT"
+
+#git submodule update --init --depth 1 || true
+git clone https://github.com/nmoehrle/mvs-texturing.git
+cd mvs-texturing
+mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
-
