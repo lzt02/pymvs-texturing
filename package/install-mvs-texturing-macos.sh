ae885b9ccbfeb6ba0d17f7cf17527521f35b0725
Jiahao620 <1023499614@qq.com>
2025-09-24 22:17:10 +0800

    fix: del ln-s for macos

--- package/install-mvs-texturing-macos.sh
+++ package/install-mvs-texturing-macos.sh
@@ -5,7 +5,6 @@ TBB_HOME="$REPO_ROOT"
 mkdir -p "$TBB_HOME"
 curl -L https://github.com/oneapi-src/oneTBB/releases/download/v2021.12.0/oneapi-tbb-2021.12.0-mac.tgz | tar -xz -C "$TBB_HOME"
 
-sudo ln -sf /opt/oneapi-tbb-2021.12.0/lib/libtbb*.dylib /usr/local/lib/
 
 brew install libpng zlib jpeg-turbo libtiff
 
