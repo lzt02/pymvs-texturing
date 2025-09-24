5945ebb41aacffe4e702efeddede1a100f6967b1
Jiahao620 <1023499614@qq.com>
2025-09-24 22:10:49 +0800

    fix: update tbb dir for macos

--- package/install-mvs-texturing-macos.sh
+++ package/install-mvs-texturing-macos.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -ex
-
-
-curl -L https://github.com/oneapi-src/oneTBB/releases/download/v2021.12.0/oneapi-tbb-2021.12.0-mac.tgz | tar -xz -C /opt
+REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+TBB_HOME="$REPO_ROOT"
+mkdir -p "$TBB_HOME"
+curl -L https://github.com/oneapi-src/oneTBB/releases/download/v2021.12.0/oneapi-tbb-2021.12.0-mac.tgz | tar -xz -C "$TBB_HOME"
 
 sudo ln -sf /opt/oneapi-tbb-2021.12.0/lib/libtbb*.dylib /usr/local/lib/
 
@@ -18,5 +19,5 @@ mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release \
          -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
          -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
-         -DTBB_ROOT=/opt/oneapi-tbb-2021.12.0
+         -DTBB_DIR="$TBB_HOME/oneapi-tbb-2021.12.0/lib/cmake/tbb"
 make -j$(sysctl -n hw.ncpu)
