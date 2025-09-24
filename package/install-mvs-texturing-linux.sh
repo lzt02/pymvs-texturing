ae82e88fb0fb0cc7264ac978237894498a55df7e
Jiahao620 <1023499614@qq.com>
2025-09-24 16:40:46 +0800

    ci: del before-all in workflows

--- package/install-mvs-texturing-linux.sh
+++ package/install-mvs-texturing-linux.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -ex      
+curl -L https://github.com/oneapi-src/oneTBB/releases/download/v2021.12.0/oneapi-tbb-2021.12.0-lin.tgz | tar -xz -C /opt
+echo "/opt/oneapi-tbb-2021.12.0/lib/intel64/gcc4.8" >/etc/ld.so.conf.d/tbb.conf && ldconfig
+dnf install -y libpng-devel zlib-devel libjpeg-devel libtiff-devel
+export CMAKE_PREFIX_PATH=/opt/oneapi-tbb-2021.12.0:$CMAKE_PREFIX_PATH
 ROOT="$(dirname "$0")/.."
 EXT="$ROOT/external"
 mkdir -p "$EXT"
